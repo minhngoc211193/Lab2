e35674f9203670fb711861e5f059a40404706a9e
Vu Thi Minh Ngoc <ngocvtmgch211193@fpt.edu.vn>
2023-11-16 11:28:18 +0700

    new

--- Lab2/Web.js
+++ Lab2/Web.js
@@ -1,7 +1,40 @@
 const http = require('http')
+const fs = require('fs')
 http.createServer((request, respond) =>{
   //  respond.end('<h1>Hello</h1>')
   respond.setHeader('Content-Type', 'application/json')
-  respond.write('<h1>Hello hi</h1>' )
-  respond.end()
-}).listen(5000)
+  if(request.url ==='/'){
+    fs.readFile('hello.html', (error, data)=> {
+        if(error){
+            console.error(error.message)
+            respond.end()
+        }
+      })  
+  }else if(request.url === '/hello'){
+    fs.readFile('hello.html', (error, data)=> {
+        if(error){
+            console.error(error.message)
+        }else{
+            respond.write(data)
+            respond.end()
+        }
+      })
+  }
+  else if(request.url === '/welcome'){
+    fs.readFile('hello.html', (error, data)=> {
+        if(error){
+            console.error(error.message)
+        }else{
+            respond.write(data)
+            respond.end()
+        }
+      })
+  }else{
+        respond.write(`<h1>Error</h1>`)
+        respond.end()
+}
+  
+})
+Server.listen(port, ()=>{
+    console.log(`http://localhost:${port}`)
+})
